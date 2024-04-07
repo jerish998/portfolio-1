c18272ded1ae73117e08ed34f1487a04e02ae762
Jerish s mathew <128453069+jerish998@users.noreply.github.com>
2024-04-07 11:37:10 +0530

    Update Intro.js

--- components/Intro.js
+++ components/Intro.js
@@ -10,7 +10,7 @@ const Intro = () => {
                     Hi, I am Megha V
                 </h1>
                 <p className={styles.description}>
-                    I am a Junior software Engineer, currently working at IBIL Solutions.</p>
+                    I am a Junior software Engineer, currently working at IBIL Solutions. thank you </p>
                 <a href = "mailto:meghavayath2001@gmail.com" className={styles.contactButton}>Contact Me</a>
             </div>
             <img src="/intro.png" alt="image" className={styles.image} />
